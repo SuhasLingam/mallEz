47108a58b67b6a4c089a62cfed49a2f9e30cce88
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2024-10-16 21:46:54 +0530

    .

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from "react";
 import HomeImage from "../assets/homeElement-1.svg";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
 import Button from "./button";
 
-const fadeInUp = {
-  hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0, transition: { duration: 1 } },
-};
-
 const fadeInLeft = {
   hidden: { opacity: 0, x: -100 },
-  visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+  visible: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeOut" } },
+  exit: { opacity: 0, x: -100, transition: { duration: 0.5, ease: "easeIn" } },
 };
 
-const fadeImage = {
+const fadeInRight = {
   hidden: { opacity: 0, x: 100 },
-  visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+  visible: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeOut" } },
+  exit: { opacity: 0, x: 100, transition: { duration: 0.5, ease: "easeIn" } },
+};
+
+const fadeInUp = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } },
+  exit: { opacity: 0, y: 50, transition: { duration: 0.5, ease: "easeIn" } },
 };
 
 const HeroSection = () => {
@@ -26,6 +30,11 @@ const HeroSection = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [typingSpeed, setTypingSpeed] = useState(100);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+    threshold: 0.1,
+  });
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -34,6 +43,14 @@ const HeroSection = () => {
 
     return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
 
   const words = ["SHOP", "NAVIGATE", "PARK"];
   const colors = ["#000D49", "#02BD08", "#9502AF"];
@@ -67,13 +84,16 @@ const HeroSection = () => {
   }, [displayedText, isDeleting, typingSpeed, currentWordIndex]);
 
   return (
-    <div className="relative min-h-screen bg-mainBackgroundColor p-4 md:p-8">
+    <div
+      ref={ref}
+      className="relative min-h-screen bg-mainBackgroundColor p-4 md:p-8"
+    >
       <div className="container mx-auto flex flex-col items-center lg:flex-row lg:justify-between">
         <motion.div
           className="mb-8 text-center lg:mb-0 lg:w-1/2 lg:text-center"
           variants={fadeInLeft}
           initial="hidden"
-          animate="visible"
+          animate={controls}
         >
           <h1 className="mb-4 text-5xl font-bold sm:text-6xl lg:text-7xl">
             Feel The Ease,
@@ -89,17 +109,22 @@ const HeroSection = () => {
           </h1>
 
           {!isLoggedIn && (
-            <div className="flex justify-center gap-4 lg:justify-center">
+            <motion.div
+              className="flex justify-center gap-4 lg:justify-center"
+              variants={fadeInUp}
+              initial="hidden"
+              animate={controls}
+            >
               <Button text="LOGIN" redirectTo="/login" />
               <Button text="SIGNUP" redirectTo="/signup" />
-            </div>
+            </motion.div>
           )}
 
           <motion.p
             className="mt-8 font-poppins text-base font-semibold italic text-mainTextColor sm:text-lg lg:text-2xl"
             variants={fadeInUp}
             initial="hidden"
-            animate="visible"
+            animate={controls}
           >
             Imagine effortlessly navigating the mall during the busy holiday
             season, with MallEZ guiding you directly to your favorite stores and
@@ -110,9 +135,9 @@ const HeroSection = () => {
 
         <motion.div
           className="lg:w-1/2"
-          variants={fadeImage}
+          variants={fadeInRight}
           initial="hidden"
-          animate="visible"
+          animate={controls}
         >
           <img src={HomeImage} alt="Mall Illustration" className="w-full" />
         </motion.div>
